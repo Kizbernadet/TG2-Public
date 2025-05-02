4837615951589923b54e67dddc484b48b89ddb16
Kizbernadet <kizbernadet4@gmail.com>
2025-05-02 15:24:47 +0000

    Conception de la dashboard

---
+++ front/js/index.js
@@ -0,0 +1,9 @@
+/* LuxeCar Script Code */
+const burger_button = document.querySelector(".menu_button")
+const navbar = document.querySelector(".navbar") 
+
+// Detection du click
+burger_button.addEventListener("click", () =>{
+    burger_button.classList.toggle("active")
+    navbar.classList.toggle("active")
+})

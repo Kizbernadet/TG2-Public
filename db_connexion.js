05755c481c7b6f2b81a24f7e897d3957722949f2
Fata-Harber <fatadjiteye5@gmail.com>
2025-05-05 23:39:05 +0000

    connexion à une base de données pour exemple

--- db_connexion.js
+++ db_connexion.js
@@ -3,11 +3,28 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '',
-  database: '',
+  //password: '',
+  database: 'ecommerce',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
-module.exports = pool;
+//module.exports = pool;
+
+
+//const pool = require('./db_connexion'); // Import du pool défini dans db_connexion.js
+
+async function testConnexion() {
+  try {
+    // Utilisation du pool pour exécuter une requête
+    const [rows] = await pool.query('SELECT 1 + 1 AS resultat');
+
+    console.log(' Connexion réussie via pool !');
+    console.log('Résultat de la requête :', rows[0].resultat);
+  } catch (err) {
+    console.error('Erreur de connexion ou de requête :', err.message);
+  }
+}
+
+testConnexion();
